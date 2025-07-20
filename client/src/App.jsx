1460fadd099e5ab59788c488faf01f9ac90d6a32
sanjayontech <sanjayontech@gmail.com>
2025-07-20 20:14:12 +0530

    created pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Layout from './pages/Layout'
+import Dashboard from './pages/Dashboard'
+import WriteArticle from './pages/Write-Article'
+import BlogTitles from './pages/BlogTitles'
 
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/ai' element={<Layout />} />
+        <Route index element={<Dashboard/>} />
+        <Route path='write-article' element={<WriteArticle />} />
+        <Route path='blog-titles' element={<BlogTitles />} />
+      </Routes>
     </div>
   )
 }

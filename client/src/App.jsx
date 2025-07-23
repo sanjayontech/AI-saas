a695782ec4a51521450677201d45ff80afc09cac
sanjayontech <sanjayontech@gmail.com>
2025-07-23 19:57:13 +0530

    Rectified home and dashboard issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Layout from './pages/Layout'
 import Dashboard from './pages/Dashboard'
-import WriteArticle from './pages/Write-Article'
+import WriteArticle from './pages/WriteArticle'
 import BlogTitles from './pages/BlogTitles'
 
 const App = () => {
@@ -11,10 +11,11 @@ const App = () => {
     <div>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/ai' element={<Layout />} />
-        <Route index element={<Dashboard/>} />
-        <Route path='write-article' element={<WriteArticle />} />
-        <Route path='blog-titles' element={<BlogTitles />} />
+        <Route path='/ai' element={<Layout />}>
+          <Route index element={<Dashboard/>} />
+          <Route path='write-article' element={<WriteArticle />} />
+          <Route path='blog-titles' element={<BlogTitles />} />
+        </Route>
       </Routes>
     </div>
   )
